d3e5a165275d22735711a47add6ea4f50511f797
AbdullahAbdelaziz122 <abdullah.abdelaziz15@gmail.com>
2026-02-18 23:25:19 +0200

    finished UserDetails service

---
+++ src/main/java/com/starter/template/starter/repositories/UserRepository.java
@@ -0,0 +1,17 @@
+package com.starter.template.starter.repositories;
+
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.starter.template.starter.models.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID>{
+
+    Optional<User> findByEmail(String email);
+    
+} 
--- src/main/java/com/starter/template/starter/security/SecurityUserDetailsService.java
+++ src/main/java/com/starter/template/starter/security/SecurityUserDetailsService.java
@@ -1,5 +1,23 @@
 package com.starter.template.starter.security;
 
-public class SecurityUserDetailsService {
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import com.starter.template.starter.models.User;
+import com.starter.template.starter.repositories.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class SecurityUserDetailsService implements UserDetailsService{
+
+    private final UserRepository repository;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = repository.findByEmail(email)
+        .orElseThrow(() -> new UsernameNotFoundException("User not Found with email: "+email));
+        return new SecurityUser(user);
+    }
+    
 }
