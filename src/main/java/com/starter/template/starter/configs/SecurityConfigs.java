bd14425aded0c1c2778a0017b831262198db64cc
AbdullahAbdelaziz122 <abdullah.abdelaziz15@gmail.com>
2026-02-18 23:34:51 +0200

    Addded intitial Security Configuration

---
+++ src/main/java/com/starter/template/starter/configs/SecurityConfigs.java
@@ -0,0 +1,49 @@
+package com.starter.template.starter.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.starter.template.starter.repositories.UserRepository;
+import com.starter.template.starter.security.SecurityUserDetailsService;
+
+@Configuration
+public class SecurityConfigs {
+
+
+    @Bean
+    public UserDetailsService userDetailsService (UserRepository userRepository){
+        return new SecurityUserDetailsService(userRepository);
+    }
+
+    @Bean
+    public SecurityFilterChain SecurityFilterChain(HttpSecurity http) throws Exception{
+        http
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers(HttpMethod.POST, "/api/v1/auth/**").permitAll()
+            .anyRequest().authenticated()
+        )
+        .csrf(csrf -> csrf.disable())
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
+    }
+}
